e7e89c3bab1b8fc37412cb4f5fbd8928144653b9
shulin59 <shulin59@gmail.com>
2020-11-30 12:36:07 +0530

    update on 30thNov

--- src/component/Place.js
+++ src/component/Place.js
@@ -36,38 +36,80 @@ export default function Place(){
             <div className="container"><br/>
             <div className="row row-cols-1 row-cols-md-3 body-pl">
                 <div className="col mb-4">
-                    <div className="card box-pl">
-                    <img src={pl1} className="card-img-top" alt="Hampi"/>
+                    <div className="card box-pl hovereffect">
+                        <img src={pl1} className="card-img-top img-responsive" alt="Hampi"/>
+                        <div class="overlay">
+                            <h2>Hampi, India</h2>
+                            <p>
+                                <span>Sree Virupaksha Temple<br/> Canon 450D</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-pl">
-                    <img src={pl2} className="card-img-top" alt="Hampi INR50"/>
+                    <div className="card box-pl hovereffect">
+                        <img src={pl2} className="card-img-top" alt="Hampi INR50"/>
+                        <div class="overlay">
+                            <h2>Hampi, India</h2>
+                            <p>
+                                <span>Stone Chariot, Vitthala Temple<br/>Canon 450D</span>
+                            </p>
+                        </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-pl">
+                    <div className="card box-pl hovereffect">
                     <img src={place} className="card-img-top" alt="Hampi"/>
+                    <div class="overlay">
+                        <h2>Hampi, India</h2>
+                        <p>
+                            <span>Sree Virupaksha Temple<br/>Canon 450D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-pl pl3">
+                    <div className="card box-pl pl3 hovereffect">
                     <img src={pl3} className="card-img-top" alt="Pondicherry"/>
+                    <div class="overlay">
+                        <h2>Pondicherry, India</h2>
+                        <p>
+                            <span>Rock Beach<br/>Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-pl">
+                    <div className="card box-pl hovereffect">
                     <img src={pl5} className="card-img-top" alt="Hampi"/>
+                    <div class="overlay">
+                        <h2>Hampi, India</h2>
+                        <p>
+                            <span>Hemakuta Hill<br/>Canon 450D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-pl pl4">
+                    <div className="card box-pl pl4 hovereffect">
                     <img src={pl4} className="card-img-top" alt="Pondicherry"/>
+                    <div class="overlay">
+                        <h2>Pondicherry, India</h2>
+                        <p>
+                            <span>Rock Beach<br/>Canon 650D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
                 <div className="col mb-4">
-                    <div className="card box-pl pl6">
+                    <div className="card box-pl pl6 hovereffect">
                     <img src={pl6} className="card-img-top" alt="Mirissa"/>
+                    <div class="overlay">
+                        <h2>Mirissa, SriLanka</h2>
+                        <p>
+                            <span>Indian Ocean<br/>Canon 450D</span>
+                        </p>
+                    </div>
                     </div>
                 </div>
             </div>
